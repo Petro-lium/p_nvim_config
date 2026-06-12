fc3e1f2e24bc32340ac8643b37962cff545a525a
Стрыгин Сергей <strygin@crtech.ru>
2026-06-12 17:31:57 +0300

    add sqlfluff

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,33 +1,43 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        lazy = false,
-        config = function()
-            local conform = require("conform")
+	{
+		"stevearc/conform.nvim",
+		lazy = false,
+		config = function()
+			local conform = require("conform")
 
-            conform.setup({
-                formatters_by_ft = {
-                    lua = { "stylua" },
-                    python = { "ruff_fix", "ruff_format" },
-                    html = { "djlint" },
-                    javascript = { "prettier" },
-                    javascriptreact = { "prettier" },
-                    typescript = { "prettier" },
-                    typescriptreact = { "prettier" },
-                    json = { "prettier" },
-                    css = { "prettier" },
-                    yaml = { "prettier" },
-                    markdown = { "prettier" },
-                },
-                format_on_save = {
-                    timeout_ms = 1000,
-                    lsp_fallback = true,
-                },
-            })
+			conform.setup({
+				formatters = {
+					sqlfluff = {
+						command = "sqlfluff",
+						-- аргументы: диалект postgres, читать stdin
+						args = { "fix", "--dialect", "postgres", "-" },
+						-- если диалект нужно менять для разных проектов, можно убрать "--dialect"
+						-- и создать файл .sqlfluff в корне проекта
+					},
+				},
+				formatters_by_ft = {
+					lua = { "stylua" },
+					python = { "ruff_fix", "ruff_format" },
+					html = { "djlint" },
+					javascript = { "prettier" },
+					javascriptreact = { "prettier" },
+					typescript = { "prettier" },
+					typescriptreact = { "prettier" },
+					json = { "prettier" },
+					css = { "prettier" },
+					yaml = { "prettier" },
+					markdown = { "prettier" },
+					sql = { "sqlfluff" },
+				},
+				format_on_save = {
+					timeout_ms = 1000,
+					lsp_fallback = true,
+				},
+			})
 
-            vim.keymap.set("n", "<leader>gf", function()
-                conform.format({ lsp_fallback = true })
-            end, { desc = "format: run formatter" })
-        end,
-    },
+			vim.keymap.set("n", "<leader>gf", function()
+				conform.format({ lsp_fallback = true })
+			end, { desc = "format: run formatter" })
+		end,
+	},
 }
