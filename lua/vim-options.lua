19211f7e051cd21ed03c9dda1a9c73cb551196ee
Стрыгин Сергей <strygin@crtech.ru>
2026-06-13 09:53:40 +0300

    добавлена горячие клавиши для работы с терминалом

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -13,6 +13,12 @@ vim.keymap.set("n", "<c-j>", ":wincmd j<CR>", { desc = "window: down" })
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>", { desc = "window: left" })
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>", { desc = "window: right" })
 
+-- Terminal escape
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "terminal: exit to normal mode" })
+
+-- Quick terminal
+vim.keymap.set("n", "<leader>pt", ":terminal powershell<CR>", { desc = "terminal: open powershell" })
+
 vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", { desc = "clear search highlight" })
 vim.wo.number = true
 
